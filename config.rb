119acb7e13bf9f2499b8475a592607b2d737deb2
Mica Semrick <mica@silentumbrella.com>
2016-03-13 15:33:33 -0700

    Activate relative asses to help allivate broken links when using github pages.

--- config.rb
+++ config.rb
@@ -96,6 +96,8 @@ activate :autoprefixer do |config|
   config.browsers = ['last 2 versions', 'ie >= 9', 'and_chr >= 2.3']
 end
 
+set :relative_links, true
+
 set :css_dir, 'stylesheets'
 
 set :js_dir, 'javascripts'
@@ -120,7 +122,7 @@ configure :build do
   # activate :asset_hash
 
   # Use relative URLs
-  # activate :relative_assets
+  activate :relative_assets
 
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
